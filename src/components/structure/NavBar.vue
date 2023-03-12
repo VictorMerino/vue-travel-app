d074e94a2192ce801932158084d1441312cefd32
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-03-12 17:34:10 +0100

    NavBar

---
+++ src/components/structure/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="nav" id="nav">
+    <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+    <RouterLink :to="{ name: 'Brazil' }">Brazil</RouterLink>
+    <RouterLink :to="{ name: 'Jamaica' }">Jamaica</RouterLink>
+    <RouterLink :to="{ name: 'Panama' }">Panama</RouterLink>
+    <RouterLink :to="{ name: 'Hawaii' }">Hawaii</RouterLink>
+    <RouterLink :to="{ name: 'About' }">About</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>

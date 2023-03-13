295810c91117a7836a03a5a060d2bc2df244480b
Víctor Merino <victor.merino@jungheinrich.es>
2023-03-13 22:50:52 +0100

    BackButton and favicon added

---
+++ src/components/GoBackButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter()
+function goBack() {
+  router.back()
+}
+</script>
+
+<template>
+  <div class="go-back">
+    <button @click="goBack">Go back</button>
+  </div>
+</template>

35efa10ec62eab5f1a90e4d35b66115520a1f7e8
Víctor Merino <victor.merino@jungheinrich.es>
2023-03-13 23:19:46 +0100

    Add simple route transitions

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,24 @@ const dos = 2
   <NavBar />
 
   <div class="container">
-    <RouterView />
+
+    <RouterView v-slot="{ Component }">
+      <Transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
+
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform .5s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%)
+}
+</style>
--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ExperienceCard from '@/components/ExperienceCard.vue';
+import GoBackButton from '@/components/GoBackButton.vue';
 import { getData } from '@/services/getData'
 import { Destination } from '@/types/Destination';
 import { computed, onBeforeMount, ref, watch } from 'vue'
@@ -25,23 +26,26 @@ onBeforeMount(async () => {
 
 </script>
 <template>
-  <template v-if="destination">
-    <section class="destination">
-      <h1>{{ destination.name }}</h1>
-      <div class="destination-details">
-        <img :src="`/images/${destination.image}`" :alt="destination.name" />
-        <p>{{ destination.description }}</p>
-      </div>
-    </section>
-    <section class="experiences">
-      <div class="cards">
-        <RouterLink :to="{ name: 'Experience', params: { slug: destination.slug, experienceSlug: experience.slug } }"
-          v-for="experience in destination.experiences" :key="experience.slug">
-          <ExperienceCard :experience="experience" />
-        </RouterLink>
+  <div>
+    <GoBackButton />
+    <template v-if="destination">
+      <section class="destination">
+        <h1>{{ destination.name }}</h1>
+        <div class="destination-details">
+          <img :src="`/images/${destination.image}`" :alt="destination.name" />
+          <p>{{ destination.description }}</p>
+        </div>
+      </section>
+      <section class="experiences">
+        <div class="cards">
+          <RouterLink :to="{ name: 'Experience', params: { slug: destination.slug, experienceSlug: experience.slug } }"
+            v-for="experience in destination.experiences" :key="experience.slug">
+            <ExperienceCard :experience="experience" />
+          </RouterLink>
 
-      </div>
-    </section>
-    <RouterView />
-  </template>
+        </div>
+      </section>
+      <RouterView />
+    </template>
+  </div>
 </template>

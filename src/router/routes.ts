2748b947655607858898892a3fc582f57797603a
Víctor Merino <victor.merino@jungheinrich.es>
2023-03-14 13:29:24 +0100

    Remove old routes and views (CHECK this commit for nested routes in different pages)

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -26,26 +26,6 @@ export const routes: RouteRecordRaw[] = [
     name: 'About',
     component: () => import('@/views/AboutView.vue'),
   },
-  // Old ugly routes
-  {
-    path: '/destination/:slug',
-    props: true,
-    children: [
-      {
-        path: '',
-        name: 'Destination_old',
-        component: () => import('@/views/ugly-way/DestinationView.vue'),
-        props: true,
-      },
-      {
-        path: 'experience/:experienceSlug',
-        name: 'Experience_old',
-        component: () => import('@/views/ugly-way/ExperienceView.vue'),
-        props: true,
-      },
-    ]
-  },
-  // REAL Nested Ones
   {
     path: '/:slug',
     name: 'Destination',

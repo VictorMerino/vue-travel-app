5e4ed8ebabdb5787f76108913978995ff9e4aff0
Víctor Merino <victor.merino@jungheinrich.es>
2023-03-14 09:20:26 +0100

    Logout button

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const username = ref(window.user)
+function logout() {
+  window.user = null
+  router.push({ name: 'Home' })
+}
+</script>
 <template>
-  <h1>Profile</h1>
+  <section class="profile">
+    <h1>Profile of {{ username }}</h1>
+    <button @click="logout">Logout</button>
+  </section>
 </template>
 
 <style scoped></style>

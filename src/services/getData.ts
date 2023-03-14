c9a559ecd8ded1d3919c8759df16fee545a91bb0
Víctor Merino <victor.merino@jungheinrich.es>
2023-03-14 17:20:16 +0100

    Now we can get all dataset or just the desired one (as the app is little, seems like a good approach to get all and filter it in client side)

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,11 +1,32 @@
 import sourceData from '@/data/data.json'
 
-export async function getData(slug: string) {
+export async function getData(slug?: string) {
   try {
-    const response = await fetch(
-      `https://travel-dummy-api.netlify.app/${slug}.json`
-    )
-    return await response.json()
+    const baseUrl = 'https://travel-dummy-api.netlify.app/'
+    let response;
+    if(slug) {
+      response = await fetch(
+        `${baseUrl}${slug}.json`
+      )
+      return await response.json()
+    }
+      // As we don't have a full data endpoint, this needs to be "hardcoded"
+      const possibleDestinations = [
+        'brazil',
+        '/hawaii',
+        '/panama',
+        '/jamaica'
+      ]
+      const urlsArray = possibleDestinations.map(
+        destination => `${baseUrl}${destination}.json`)
+
+      const promisedResponses = await Promise.all(
+        urlsArray.map(async url => await fetch(url)));
+
+      response = await Promise.all(
+        promisedResponses.map(response => response.json()))
+
+      return response
   }
   catch(err) {
     console.log(err)
